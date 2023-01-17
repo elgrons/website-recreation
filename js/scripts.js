cfb4b0b4f9f1baed518e7b75b77857646b733503
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-17 13:48:27 -0800

    add img tag and photo using .setAttribute and .appendChild.
    Co-Authored-By: Erin Timlin <erintimlin@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 window.onload = function() {
-  //event.preventDefault()
   
   const favoriteThings = document.querySelector("h1");
   favoriteThings.remove();
 
   const hElement = document.createElement("h1"); 
-  hElement.append("Webpage Recreation Practice")
-  console.log("h1",hElement);
+  hElement.append("Webpage Recreation Practice");
   document.querySelector("body").append(hElement);
+
+  const pElement = document.createElement("p");
+  pElement.append("The HTML of this webpage was created wtih JavaScript.")
+  document.querySelector("body").append(pElement);
+
+  // const imgElement = document.createElement("img");
+  // imgElement.append("src=https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900");
+  // document.querySelector("body").append(imgElement);
+
+const imgElement = document.createElement("img");
+imgElement.setAttribute('src', "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900");
+imgElement.setAttribute('alt', "This is an image of a Multicolored Tanager bird from birdsoftheworld.org");
+imgElement.setAttribute('width', '50%');
+document.body.appendChild(imgElement);
 };
