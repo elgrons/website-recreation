bef57bff59658eb52bd162d2df824df2c86efd4f
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-17 14:00:37 -0800

    use createElement to add ul and li.

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,23 @@ window.onload = function() {
   pElement.append("The HTML of this webpage was created wtih JavaScript.")
   document.querySelector("body").append(pElement);
 
-  // const imgElement = document.createElement("img");
-  // imgElement.append("src=https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900");
-  // document.querySelector("body").append(imgElement);
+  const imgElement = document.createElement("img");
+  imgElement.setAttribute('src', "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900");
+  imgElement.setAttribute('alt', "This is an image of a Multicolored Tanager bird from birdsoftheworld.org");
+  imgElement.setAttribute('width', '50%');
+  document.body.appendChild(imgElement);
 
-const imgElement = document.createElement("img");
-imgElement.setAttribute('src', "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900");
-imgElement.setAttribute('alt', "This is an image of a Multicolored Tanager bird from birdsoftheworld.org");
-imgElement.setAttribute('width', '50%');
-document.body.appendChild(imgElement);
+  const h1Element = document.createElement("h1"); 
+  h1Element.append("Facts about the Multicolored Tanager");
+  document.querySelector("body").append(h1Element);
+
+  const ul = document.createElement("ul");
+  ul.setAttribute("id", "tanagerFacts");
+  document.querySelector("body").append(ul);
+  const liOne = document.createElement ("li");
+  const liTwo = document.createElement ("li");
+  liOne.append("It is endemic to the mountains of Colombia.");
+  liTwo.append("It usually searches for insects on the underside of leaves of outer limbs while clinging to leaves with its feet.");
+  document.getElementById("tanagerFacts").append(liOne, liTwo);
+
 };
